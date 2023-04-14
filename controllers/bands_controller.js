a0145ad43ac98588b3b5bdf1a8f0b15b431d940f
Tara Maddox <TMaddox85@gmail.com>
2023-04-14 09:15:00 -0700

    6 act 2

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -1,16 +1,19 @@
 // DEPENDENCIES
 const bands = require('express').Router()
 const db = require('../models')
-const { Band } = db 
+const { Band, MeetGreet, Event, SetTime } = db 
 const {Op} = require('sequelize')
 
 // FIND ALL BANDS
-bands.get('/', async (req, res) => {
+bands.get('/:name', async (req, res) => {
     try {
         const foundBands = await Band.findAll({
-            order: [['available_start_time', 'ASC']],
-            where: {
-                name: { [Op.like] : `%$(req.query.name ? req.query.name : '')%`}
+            //order: [['available_start_time', 'ASC']],
+            where: {name: req.params.name},
+            include: {
+                model: MeetGreet,
+                as: "meet_greets",
+                include:{ model: Event, as:"event"}
             }
         })
         res.status(200).json(foundBands)
@@ -20,10 +23,30 @@ bands.get('/', async (req, res) => {
 })
 
 // FIND A SPECIFIC BAND
-bands.get('/:id', async (req, res) => {
+bands.get('/:name', async (req, res) => {
     try {
         const foundBand = await Band.findOne({
-            where: { band_id: req.params.id }
+            where: { name: req.params.name },
+            include: [
+                {
+                    model: MeetGreet,
+                    as: "meet_greets",
+                    include: { 
+                    model: Event, 
+                    as: "event",
+                    where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%`} }
+                    }
+                },
+                {
+                    model: SetTime,
+                    as: "set_times",
+                    include: { 
+                        model: Event, 
+                        as: "event",
+                        where: { name: { [Op.like] : `%${req.query.event ? req.query.event : ''}%`}}
+                    }
+                }
+            ]
         })
         res.status(200).json(foundBand)
     } catch (error) {
